7de35ac12075feb7c9f9aec092c4d4677bacdd79
unknown <jacksonkupris@gmail.com>
2020-09-16 18:40:06 -0500

    more environs

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = 'r-i$(@lt@(8s(@xqyz(+wojfwl7l_hrsa%b=thqqrl58j%h&cn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
