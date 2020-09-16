fa3e41436fc7679eea82e72d9d18653a78fd480c
unknown <jacksonkupris@gmail.com>
2020-09-16 00:44:04 -0500

    Heroku changes

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import whitenoise
+import django_heroku
 
 
 
@@ -173,3 +174,5 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_heroku.settings(locals())
