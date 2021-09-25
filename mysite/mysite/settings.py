4da1e66919dcef1b13977e540fd4643a3ff34c58
unknown <jacksonkupris@gmail.com>
2021-09-24 20:17:23 -0500

    Forgot to rename resume

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'r-i$(@lt@(8s(@xqyz(+wojfwl7l_hrsa%b=thqqrl58j%h&cn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
